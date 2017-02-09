b75f6a11969838cd5b890d438af8cb6b19afd852
Nick Jones <najones19746@pluto.dsu.edu>
2017-02-08 18:19:54 -0600

    oh god why

--- testing.py
+++ testing.py
@@ -1,9 +1,10 @@
 __author__ = 'killo'
-import enchant
 import itertools
 import re
 import time
+import random
 import collections
+import ngram_score
 
 characterUseOrder = ['e','t','a','o','i','n','s','h','r','d']
 
@@ -33,7 +34,46 @@ def mostUsedCharacters(string,numChars):
 class smartCipher:
     def __init__(self, cipherStrings):
         self.cipherStrings = cipherStrings
-        self.matchCount = len( set( self.cipherStrings[0]) & set(self.cipherStrings[1]) )
+        self.matchSet = set( self.cipherStrings[0]) & set(self.cipherStrings[1])
+        self.matchCount = len(self.matchSet)
+        self.firstCipherMatchPositions = []
+        self.secondCipherMatchPositions = []
+        self.firstCipherPattern = self.convertString( cipherStrings[0] )
+        self.secondCipherPattern = self.convertString( cipherStrings[1] )
+        self.firstPossibleWords = []
+        self.secondPossibleWords = []
+        for item in self.matchSet:
+            self.firstCipherMatchPositions.append([index for index, char in enumerate(self.cipherStrings[0]) if char in item])
+            self.secondCipherMatchPositions.append([index for index, char in enumerate(self.cipherStrings[1]) if char in item])
+
+        patternFile = open( ("wordsPatterns" + str(len(self.cipherStrings[0]))), "r")
+        for line in patternFile:
+            wordPattern = line.rstrip().split(',')
+            if wordPattern[1] == self.firstCipherPattern:
+                self.firstPossibleWords.append(wordPattern[0])
+        patternFile.close()
+
+        patternFile = open( ("wordsPatterns" + str(len(self.cipherStrings[1]))), "r")
+        for line in patternFile:
+            wordPattern = line.rstrip().split(',')
+
+            if wordPattern[1] == self.secondCipherPattern:
+                self.secondPossibleWords.append(wordPattern[0])
+        patternFile.close()
+
+
+    def convertString(self,string):
+        patternLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z']
+        string = string.rstrip()
+        foundChars = []
+        computedPattern = ""
+        for character in string:
+            if character not in foundChars:
+                foundChars.append(character)
+            computedPattern += patternLetters[foundChars.index(character)]
+        return computedPattern
+
+
         
 
 #ceasar bullshit for warmup
@@ -53,9 +93,89 @@ def ceaser(string, step):
 
 #B E G I N
 
-alphaString = "abcdefghijklmnopqrstuvwxyz"
+alphaString = list("abcdefghijklmnopqrstuvwxyz".upper())
+cipher = "tpfccdlfdtte pcaccplircdt dklpcfrp?qeiq lhpqlipqeodf gpwafopwprti izxndkiqpkii krirrifcapnc dxkdciqcafmd vkfpcadf".replace(' ', '').replace('?', '').upper()
 
-cipher = "tpfccdlfdtte pcaccplircdt dklpcfrp?qeiq lhpqlipqeodf gpwafopwprti izxndkiqpkii krirrifcapnc dxkdciqcafmd vkfpcadf"
+
+OGKEY = list('abcdefghijklmnopqrstuvwxyz'.upper())
+topChars = mostUsedCharacters(cipher, 5)
+print topChars
+
+
+maxscore = -99e9
+parentscore,parentkey = maxscore, OGKEY[:]
+plainFitness = ngram_score.ngram_score('english_quadgrams.txt')
+# keep going until we are killed by the user
+i = 0
+while 1:
+    i = i+1
+    random.shuffle(parentkey)
+    deciphered = simul_replace( cipher , {k: v for k, v in zip(parentkey, alphaString)})
+
+    parentscore = plainFitness.score(deciphered)
+    count = 0
+
+    for i in range(0, len(topChars)):
+            parentkey[ parentkey.index( topChars[i] )], parentkey[ alphaString.index(topChars[i]) ] = parentkey[ alphaString.index(topChars[i]) ],parentkey[ parentkey.index( topChars[i] )]
+
+    while count < 1500:
+        a = random.randint(0,25)
+        b = random.randint(0,25)
+        child = parentkey[:]
+        # swap two characters in the child
+        child[a],child[b] = child[b],child[a]
+        deciphered = simul_replace( cipher , {k: v for k, v in zip(child, alphaString)})
+
+        score = plainFitness.score(deciphered)
+        # if the child was better, replace the parent with it
+        if score > parentscore:
+            parentscore = score
+            parentkey = child[:]
+            count = 0
+        count = count+1
+    # keep track of best score seen so far
+    if parentscore>maxscore:
+        maxscore, OGKEY = parentscore, parentkey[:]
+        print '\nbest score so far:',maxscore,'on iteration',i
+        print '    best key: '+''.join(OGKEY)
+        print '    plaintext: '+  simul_replace( cipher , {k: v for k, v in zip(OGKEY, alphaString)})
+
+
+
+'''
+i = 0
+maxScore = -99999999999999999
+plainFitness = ngram_score.ngram_score('english_quadgrams.txt')
+innerLoopScore = maxScore
+while True:
+    i += 1
+    random.shuffle(keyString)
+    plaintext = simul_replace( cipher , {k: v for k, v in zip(keyString, alphaString)})
+    #print(plaintext)
+    innerLoopScore = plainFitness.score(plaintext)
+    count = 0
+    while count < 400:
+        a = random.randint(0,25)
+        b = a
+        while b == a:
+            b = random.randint(0,25)
+        childKey = keyString[:]
+        childKey[a], childKey[b] = childKey[b], childKey[a]
+        plaintext = simul_replace( cipher , {k: v for k, v in zip(childKey, alphaString)})
+        score = plainFitness.score(plaintext)
+        if score > innerLoopScore:
+            innerLoopScore = score
+            keyString = childKey[:]
+            count = 0
+        count += 1
+    if innerLoopScore > maxScore:
+        maxScore = innerLoopScore
+        OGKEY = keyString[:]
+        print 'best score:' + str(maxScore)
+        print 'plaintext: ' + simul_replace(cipher, {k: v for k, v in zip(OGKEY, alphaString)})
+
+
+'''
 
 # brute force attempt
 '''
@@ -93,8 +213,8 @@ print "finished computation, time: ", (t2-t1)
 
 #    print(ceaser(cipher,i))
 '''
-
 #smart attempt 1
+'''
 cipherFormatted = cipher.replace('?', '')
 
 print(cipherFormatted)
@@ -109,4 +229,7 @@ for item in itertools.combinations(cipherWordList, 2):
 smartCipherFrags.sort(key=lambda x: x.matchCount, reverse=True)
 
 for item in smartCipherFrags:
-    print item.cipherStrings, item.matchCount
+    print item.cipherStrings, item.matchCount, item.firstCipherMatchPositions,"::" , item.secondCipherMatchPositions
+    print item.firstCipherPattern, item.secondCipherPattern
+    print item.firstPossibleWords, item.secondPossibleWords
+'''
--- words fixer.py
+++ words fixer.py
@@ -1,11 +1,40 @@
 __author__ = 'killo'
+'''
 input = open("words.txt", "r")
-output = open("words2.txt", "w")
 
-output.write(input.readline())
+
+outputList = [0]
+for x in range(1,23):
+    outputList.append( open(("words" + str(x)), 'w+'))
+
 
 for line in input:
-    output.write(line.lower())
+    outputList[len(line) - 1].write(line)
 
 input.close()
-output.close()
+for x in range(1,23):
+    outputList[x].close()
+'''
+
+def convertString(string):
+        patternLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z']
+        string = string.rstrip()
+        foundChars = []
+        computedPattern = ""
+        for character in string:
+            if character not in foundChars:
+                foundChars.append(character)
+            computedPattern += patternLetters[foundChars.index(character)]
+        return computedPattern
+
+
+for x in range(1,23):
+    inputFile = open(("words" + str(x)), "r")
+    outputFile = open(("wordsPatterns" + str(x)), "w+")
+    words = []
+    for line in inputFile:
+        if line.rstrip().isalpha() and line.lower().rstrip() not in words:
+            outputFile.write(line.lower().rstrip() + "," +convertString(line) + "\n")
+            words.append(line.rstrip().lower())
+    inputFile.close()
+    outputFile.close()
